6012b323bae49e41df0debb5aadb241bfac041c1
TobiasH87 <tobiash87@gmail.com>
2012-02-28 15:53:52 +0100

    Server nicht erreichbar - Fehler beseitigt

--- files/files/lib/system/cache/CacheBuilderIRCUserOnline.class.php
+++ files/files/lib/system/cache/CacheBuilderIRCUserOnline.class.php
@@ -4,9 +4,9 @@ require_once(WCF_DIR.'lib/system/cache/CacheBuilder.class.php');
 
 /**
  * @author		Marco Daries
- * @copyright	2011 MaDa-Network.de
+ * @copyright	2011 - 2012 MaDa-Network.de
  * @website		mada-network.de
- * @package		de.community4wcf.irc.wcf
+ * @svn			$Id: CacheBuilderIRCUserOnline.class.php 1863 2012-02-28 14:52:27Z TobiasH87 $
  */
  
 class CacheBuilderIRCUserOnline implements CacheBuilder {
@@ -15,32 +15,30 @@ class CacheBuilderIRCUserOnline implements CacheBuilder {
 	 */
 	public function getData($cacheResource) {
 		$data = array();
-
-		//if (@fsockopen('irc.european-network.eu', 80, $errnum, $errstr, 2)){
+		
 		$sourceURL = 'http://irc.european-network.eu/index.php?page=IRCServiceReadDatas&uniqueID='.IRCUSERONLINE_UNIQUEID;
-		$filename = FileUtil::downloadFileFromHttp($sourceURL, 'ircService');
 	
-		$xml = simplexml_load_file($filename);
-
-		$count = 0;
-		foreach ($xml->channel as $channel) {			
-			$data[$count]['channelname'] = StringUtil::trim($channel->channelname);
-			$data[$count]['usercount'] = 0;			
+		$xml = @simplexml_load_file($sourceURL);
+		if(!empty($xml)) {
+			$count = 0;
+			foreach ($xml->channel as $channel) {			
+				$data[$count]['channelname'] = StringUtil::trim($channel->channelname);
+				$data[$count]['usercount'] = 0;			
 			
-			$count2 = 0;
-			foreach ($channel->useronlinelist->useronline as $useronlinelist) {								
-				$data[$count]['useronlinelists'][$count2]['nickname'] = StringUtil::trim($useronlinelist->nickname);
-				$data[$count]['useronlinelists'][$count2]['voice'] = StringUtil::trim($useronlinelist->voice);
-				$data[$count]['useronlinelists'][$count2]['op'] = StringUtil::trim($useronlinelist->op);
-				$data[$count]['usercount']++;
-				$count2++;
+				$count2 = 0;
+				foreach ($channel->useronlinelist->useronline as $useronlinelist) {								
+					$data[$count]['useronlinelists'][$count2]['nickname'] = StringUtil::trim($useronlinelist->nickname);
+					$data[$count]['useronlinelists'][$count2]['voice'] = StringUtil::trim($useronlinelist->voice);
+					$data[$count]['useronlinelists'][$count2]['op'] = StringUtil::trim($useronlinelist->op);
+					$data[$count]['usercount']++;
+					$count2++;
+				}
+			
+				$count++;
 			}
-			
-			$count++;
 		}
-		//}
 		return $data;
-	}	
+	}
 }
 
 ?>
